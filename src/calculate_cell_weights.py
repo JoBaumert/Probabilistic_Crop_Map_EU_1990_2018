3fd462a959c386363e615b31f087005ea6029b19
JoBaumert <Josef.Baumert@web.de>
2024-09-26 17:35:54 +0200

    calculation of weight works

--- src/calculate_cell_weights.py
+++ src/calculate_cell_weights.py
@@ -7,7 +7,9 @@ from rasterio import features
 import numpy as np
 import os
 from pathlib import Path
-# %%
+from src.utils.preprocess_crop_data import cluster_crop_names
+
+#%%
 main_path_data_external="/home/baumert/fdiexchange/baumert/DGPCM_19902020/Data/raw/GEE_DGPCM/"
 raw_data_path=main_path_data_external+"Raw_Data/"
 delineation_and_parameter_path=main_path_data_external+"delineation_and_parameters/"
@@ -22,8 +24,18 @@ os.makedirs(preprocessed_dir, exist_ok=True)
 #%%
 nuts_regions_dictionary=pd.read_csv(result_dir+"csv/nuts_regions_dictionary.csv")
 crop_data=pd.read_csv(preprocessed_dir+"csv/relevant_crop_data.csv")
-# %%
 NUTS_code_mapping=crop_data[["CAPRI_code","NUTS_ID"]].drop_duplicates(["CAPRI_code","NUTS_ID"])
+#%%
+"""cluster crops accordingly"""
+crop_delineation=pd.read_excel(delineation_and_parameter_path+"DGPCM_crop_delineation.xlsx")
+all_crops=np.sort(crop_delineation.dropna(subset="DGPCM_code")["DGPCM_code"].unique())
+#%%
+preprocessed_crop_data=cluster_crop_names(crop_data=crop_data,crop_delineation=crop_delineation,
+                                          all_crops=all_crops)
+#%%
+preprocessed_crop_data
+#%%
+
 #%%
 year=1990
 relevant_nuts_ids=crop_data[crop_data["year"]==year]["NUTS_ID"].unique()
@@ -69,30 +81,28 @@ years=np.array(corine_parameters["year"])
 corine_years=np.array(corine_parameters["CORINE_year"])
 corine_years_unique=np.unique(corine_years)
 #%%
+#create one 3 dimensional matrix that contains all the corine data as bands (one per year)
 corine_raw_allyears=[]
 for i,cy in enumerate(corine_years_unique):
     corine_raw_allyears.append(rio.open(raw_data_path+"CORINE_"+str(cy)+".tif").read()[0])
-#%%
+
 corine_preprocessed_allyears=np.array(corine_raw_allyears)
-#%%
 nan_indices=np.where(np.isnan(corine_preprocessed_allyears[0]))
 corine_preprocessed_allyears[0][nan_indices]=corine_preprocessed_allyears[1][nan_indices]
-# %%
+
 corine_preprocessed_19902018=[]
 for year in corine_years:
     corine_preprocessed_19902018.append(corine_preprocessed_allyears[np.where(corine_years_unique==year)[0][0]])
-# %%
+
 corine_preprocessed_19902018=np.array(corine_preprocessed_19902018)
 # %%
 corine_preprocessed_19902018.shape
 # %%
-year=1990
+
 output_raster_UAA=np.where(corine_preprocessed_19902018[np.where(years==year)[0][0]]>0,
                            output_raster,-1)
 # %%
-show(np.where(output_raster==43,1,0))
-# %%
-corine_preprocessed_19902018.shape
+
 # %%
 new_mapping_df=pd.DataFrame(new_mapping_dict)
 
@@ -135,10 +145,75 @@ for country in new_mapping_df["NUTS0"].unique():
 # %%
 new_mapping_df=new_mapping_df[~new_mapping_df["NUTS_ID"].isin(excluded_nuts_ids)]
 # %%
-relevant_cropdata=crop_data[(crop_data["year"]==year)&(crop_data["NUTS_ID"].isin(new_mapping_df["NUTS_ID"]))]
+show(np.where(np.isin(output_raster,new_mapping_df["index"].unique()),1,0))
+#%%
+relevant_cropdata=preprocessed_crop_data[
+    (preprocessed_crop_data["year"]==year)&(preprocessed_crop_data["NUTS_ID"].isin(new_mapping_df["NUTS_ID"]))]
 
 # %%
-relevant_cropdata
+relevant_cropdata.drop_duplicates(subset=["CAPRI_code","DGPCM_crop_code"],inplace=True)
+relevant_cropdata.drop("NUTS_ID",axis=1,inplace=True)
+#%%
+uaa_calculated=relevant_cropdata[["CAPRI_code","year","country","value"]].groupby(
+    ["CAPRI_code","year","country"]
+).sum().reset_index()
+
+uaa_calculated.rename(columns={"value":"UAA_CAPRI"},inplace=True)
+
+
 # %%
-NUTS_code_mapping["NUTS_ID"].value_counts()
+corine_data_relevant=corine_preprocessed_19902018[np.where(years==year)[0][0]]
+# %%
+uaa_CORINE_list=[]
+index_list_old=[]
+index_list_new=[]
+index_list_new_wo_duplicates=[]
+nuts_id_list=[]
+for i,capri_code in enumerate(np.array(uaa_calculated["CAPRI_code"])):
+    nuts_ids=np.array(NUTS_code_mapping[NUTS_code_mapping["CAPRI_code"]==capri_code]["NUTS_ID"])
+    if len(nuts_ids)==1:
+        nuts_id=nuts_ids[0]
+        index=new_mapping_df[new_mapping_df["NUTS_ID"]==nuts_id]["index"].iloc[0]
+        uaa_CORINE_list.append(np.nansum(corine_data_relevant[np.where(output_raster==index)])/10)
+        index_list_old.append(index)
+        index_list_new.append(index)
+        nuts_id_list.append(nuts_id)
+        index_list_new_wo_duplicates.append(index)
+    else:
+        uaa=0
+        index_list_new_wo_duplicates.append(1000+i)
+        for nuts_id in nuts_ids:
+            index=new_mapping_df[new_mapping_df["NUTS_ID"]==nuts_id]["index"].iloc[0]
+            uaa+=np.nansum(corine_data_relevant[np.where(output_raster==index)])/10
+            index_list_old.append(index)
+            index_list_new.append(1000+i)
+            nuts_id_list.append(nuts_id)
+        uaa_CORINE_list.append(uaa)
+
+uaa_calculated["UAA_CORINE"]=uaa_CORINE_list
+# %%
+nuts_indices_relevant=output_raster.copy()
+changed_index_positions=np.where(np.invert(np.array(index_list_old)==np.array(index_list_new)))[0]
+for i,index in enumerate(np.array(index_list_old)[changed_index_positions]):
+    nuts_indices_relevant[np.where(nuts_indices_relevant==index)]=index_list_new[changed_index_positions[i]]
+
+nuts_indices_relevant=np.where((np.isin(nuts_indices_relevant,index_list_new))&(corine_data_relevant>0),nuts_indices_relevant,-1)
+
+#%%
+uaa_calculated["index"]=index_list_new_wo_duplicates
+# %%
+uaa_raster_relevant=np.where(nuts_indices_relevant>=0,corine_data_relevant,np.nan)
+# %%
+uaa_calculated["weight_factor"]=uaa_calculated["UAA_CAPRI"]/uaa_calculated["UAA_CORINE"]
+# %%
+weight_raster=np.ones_like(uaa_raster_relevant)*(-1)
+for i,index in enumerate(np.array(uaa_calculated["index"])):
+    weight_factor=uaa_calculated["weight_factor"].iloc[i]
+    weight_raster=np.where(nuts_indices_relevant==index,uaa_raster_relevant*weight_factor,weight_raster)
+# %%
+show(np.where(weight_raster>1,1,0))
+# %%
+show(np.where(nuts_indices_relevant==87,weight_raster,0))
+# %%
+uaa_calculated
 # %%
---
+++ src/utils/preprocess_crop_data.py
@@ -0,0 +1,88 @@
+#%%
+import numpy as np
+import pandas as pd
+# %%
+def cluster_crop_names(crop_data,crop_delineation,all_crops):
+    crop_data.drop("type",inplace=True,axis=1)
+    crop_data.drop("Unnamed: 0",inplace=True,axis=1)
+    
+    calc_ocer_df1=crop_data[crop_data["crop"].isin(["SWHE","DWHE","RYEM","BARL","OATS","MAIZ"])].groupby([
+        "CAPRI_code","NUTS_ID","year","country","NUTS_level"
+    ]).sum().reset_index()
+
+    calc_ocer_df2=crop_data[crop_data["crop"]=="CERE"].groupby([
+        "CAPRI_code","NUTS_ID","year","country","NUTS_level"
+    ]).sum().reset_index()
+
+    calc_ocer_df=pd.merge(calc_ocer_df2,calc_ocer_df1,how="left",on=["CAPRI_code","NUTS_ID","year","country","NUTS_level"])
+    calc_ocer_df.fillna(0,inplace=True)
+    calc_ocer_df["value"]=calc_ocer_df["value_x"]-calc_ocer_df["value_y"]
+    calc_ocer_df["DGPCM_crop_code"]=np.repeat("OCER",len(calc_ocer_df))
+    calc_ocer_df=calc_ocer_df[["CAPRI_code","NUTS_ID","year","country","NUTS_level","DGPCM_crop_code","value"]]
+
+    calc_text_df1=crop_data[crop_data["crop"].isin(["RAPE","SUNF","SOYA"])].groupby([
+        "CAPRI_code","NUTS_ID","year","country","NUTS_level"
+    ]).sum().reset_index()
+
+    calc_text_df2=crop_data[crop_data["crop"].isin(["TEXT","OILS"])].groupby([
+        "CAPRI_code","NUTS_ID","year","country","NUTS_level"
+    ]).sum().reset_index()
+
+    calc_text_df=pd.merge(calc_text_df2,calc_text_df1,how="left",on=["CAPRI_code","NUTS_ID","year","country","NUTS_level"])
+
+    calc_text_df.fillna(0,inplace=True)
+    calc_text_df["value"]=calc_text_df["value_x"]-calc_text_df["value_y"]
+    calc_text_df["DGPCM_crop_code"]=np.repeat("TEXT",len(calc_text_df))
+    calc_text_df=calc_text_df[["CAPRI_code","NUTS_ID","year","country","NUTS_level","DGPCM_crop_code","value"]]
+
+    calc_maiz_df=crop_data[crop_data["crop"].isin(["MAIZ","MAIF"])].groupby([
+        "CAPRI_code","NUTS_ID","year","country","NUTS_level"
+    ]).sum().reset_index()
+    calc_maiz_df.drop("crop",axis=1,inplace=True)
+    calc_maiz_df["DGPCM_crop_code"]=np.repeat("MAIZ",len(calc_maiz_df))
+
+    calc_oliv_df=crop_data[crop_data["crop"].isin(["OLIV","TABO"])].groupby([
+        "CAPRI_code","NUTS_ID","year","country","NUTS_level"
+    ]).sum().reset_index()
+    calc_oliv_df.drop("crop",axis=1,inplace=True)
+    calc_oliv_df["DGPCM_crop_code"]=np.repeat("OLIV",len(calc_oliv_df))
+
+    calc_viny_df=crop_data[crop_data["crop"].isin(["VINY","TWIN"])].groupby([
+        "CAPRI_code","NUTS_ID","year","country","NUTS_level"
+    ]).sum().reset_index()
+    calc_viny_df.drop("crop",axis=1,inplace=True)
+    calc_viny_df["DGPCM_crop_code"]=np.repeat("VINY",len(calc_viny_df))
+
+    remaining_crops=crop_delineation["CAPRI_code"].unique()[np.where(crop_delineation["CAPRI_code"].unique().astype(str)!="nan")]
+    
+    remaining_crop_df=crop_data[crop_data["crop"].isin(remaining_crops)]
+    remaining_crop_df.rename(columns={"crop":"DGPCM_crop_code"},inplace=True)
+    
+    new_crop_df=pd.concat((remaining_crop_df,pd.concat((
+        calc_ocer_df,pd.concat((
+            calc_text_df,pd.concat((
+                calc_maiz_df,pd.concat((
+                    calc_oliv_df,calc_viny_df
+                    ))
+                ))
+            ))
+        ))
+    ))
+    
+    new_crop_df.sort_values(by=["year","CAPRI_code","NUTS_ID","country","DGPCM_crop_code"],inplace=True)
+    
+    new_crop_df=new_crop_df[new_crop_df["value"]>=0]
+    
+    a=new_crop_df.groupby(["CAPRI_code","NUTS_ID","year","country","NUTS_level"]).sum().reset_index()
+    a=a[["CAPRI_code","NUTS_ID","year","country","NUTS_level"]]
+
+    b=pd.DataFrame(np.repeat(np.array(a),len(all_crops),axis=0),columns=a.columns)
+    b["DGPCM_crop_code"]=np.tile(all_crops,len(a))
+
+    c=pd.merge(b,new_crop_df,how="left",
+            on=['CAPRI_code','NUTS_ID', 'year', 'country', 'NUTS_level','DGPCM_crop_code'])
+
+    c.fillna(0,inplace=True)
+    c.sort_values(by=["year","CAPRI_code","NUTS_ID","country","DGPCM_crop_code"],inplace=True)
+
+    return c
